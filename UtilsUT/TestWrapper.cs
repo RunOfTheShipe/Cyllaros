f48bbf1f66c7ecbc0ec1533887cad0a4daae0734
Brian Shipe <bmshipe@gmail.com>
2017-05-27 15:46:08 -0500

    Adding some to-do notes

--- UtilsUT/TestWrapper.cs
+++ UtilsUT/TestWrapper.cs
@@ -1,4 +1,6 @@
-﻿namespace Cyllaros.Testing.UtilsUT
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cyllaros.Testing.UtilsUT
 {
     /// <summary>
     /// Wraps the .NET test framework. Uses inheritance to expose initialization and
@@ -7,8 +9,18 @@
     /// Derived classes only need to mark test methods with the [TestMethod] attribute;
     /// all other initialization may be done by overriding virtual methods.
     /// </summary>
+    /// <remarks>
+    /// TODO@BMS:
+    ///     1. Create a test environment; setup and tear-down
+    ///         - look at some sort of TestContext
+    ///     2. Look at making sure that things are always called, even if people don't
+    ///        know how to correctly override and call base classes
+    ///     3. Look at how assembly based things work (context, initialization, clean-up?)
+    /// </remarks>
+    [TestClass]
     public abstract class TestWrapper
     {
+        [TestInitialize]
         public void Init()
         {
             TestInit();
@@ -17,6 +29,7 @@
         protected virtual void TestInit()
         { }
 
+        [TestCleanup]
         public void CleanUp()
         {
             TestCleanUp();
